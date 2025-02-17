fd7e6d675324562a8c1edc072fadd0bc43f5dcff
kryvorotenko <ilakrivorotenko@gmail.com>
2025-02-17 11:08:25 +0100

    added 5+5 logging

--- src/main/java/ua/nure/App.java
+++ src/main/java/ua/nure/App.java
@@ -6,10 +6,11 @@ package ua.nure;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("5+5=" + sum(5, 5));
     }
 
     public static int sum(int a, int b) {
-        if(a + b > 100){
+        if (a + b > 100) {
             return 100;
         }
         return a + b;
